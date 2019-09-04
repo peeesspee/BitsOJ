85a6c3754b7ae68b716732bb26ceaa5004b5ed61
sachinam1397 <sachinam1397@gmail.com>
2019-09-04 22:13:16 +0530

    Working Client for Prakhar

--- Client/client.py
+++ Client/client.py
@@ -1,7 +1,7 @@
 import pika
 from connection import manage_connection
 from login import authenticate_login
-from database_management import manage_database
+# from database_management import manage_database
 # from submission import submit_solution
 # from submission import read_solution, 
 from threading import *
@@ -12,7 +12,7 @@ rabbitmq_username = 'client'
 rabbitmq_password = 'client'
 host = 'localhost'
 
-cursor = manage_database.initialize_table()
+# cursor = manage_database.initialize_table()
 channel,connection = manage_connection.initialize_connection(rabbitmq_username,rabbitmq_password,host)
 
 client_id,username = authenticate_login.login(channel,host)
