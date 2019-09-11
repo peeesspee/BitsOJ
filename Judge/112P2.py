3939b50daf70ef4fc1aeffabf3eed533f7bc698d
ps0798 <prashant0797@gmail.com>
2019-09-11 22:41:57 +0530

    files separated

---
+++ Judge/112P2.py
@@ -0,0 +1,3 @@
+x = "ram.123"
+inr = x.split(".")[1]
+print(inr)
--- Judge/verdict.py
+++ Judge/verdict.py
@@ -1,24 +1,62 @@
 from file_creation import file_manager
 import subprocess
 
+
 class verdict():
 
 
-	x = '#include<stdio.h> \nusing namespace std; \nint main() \n { \n printf("hello world!"); \n }'
-	code = x
+	x = 'x = "ram.123"\ninr = x.split(".")[1]\nprint(inr)'
+	source_code = x
 	problem_code = 'ARCT'
-	run_id = '112' 
+	run_id = '112'
 	client_id = '1' 
-	language = 'C++'
+	language = 'PY2'
+ 
+	# x = open("123.cpp", "r")
+	# print(x.read())
+	# print("................. \n ..................")
 
-	file_name = file_manager.file_name(code, problem_code, run_id, client_id, language)
+	file_name = file_manager.file_name(run_id, problem_code, language, source_code )
 
-	file = file_manager.create_file(code, language, file_name)
+	file = file_manager.create_file(source_code, language, file_name)
 
-	tmp = subprocess.call(["g++",file])
-	tmp = subprocess.call("./a.out")
+	if  file_name.split('.')[1] == 'cpp':
+		try:
+			tmp = subprocess.call(["g++",file])
+			tmp = subprocess.call("./a.out")
+			print(tmp)
+		
+		except Exception as e :
+			print(e)
 
-	print(tmp)
+
+	if  file_name.split('.')[1] == 'c':
+		try:
+			tmp = subprocess.call(["g++",file])
+			tmp = subprocess.call("./a.out")
+			print(tmp)
+		
+		except Exception as e :
+			print(e)
+
+
+	if  file_name.split('.')[1] == 'java':
+		try:
+			tmp = subprocess.call(["g++",file])
+			tmp = subprocess.call("./a.out")
+			print(tmp)
+		
+		except Exception as e :
+			print(e)
+
+
+	if  file_name.split('.')[1] == 'py':
+		try:
+			tmp = subprocess.call(["python2",file])
+			print(tmp)
+		
+		except Exception as e :
+			print(e)
 
 	
 
