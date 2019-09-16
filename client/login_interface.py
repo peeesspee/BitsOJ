7b0cd300505c1d4c78e230358203a12f28d954c0
Valiant1011 <prakharp789@gmail.com>
2019-09-16 21:34:45 +0530

    Logingui

--- client/login_interface.py
+++ client/login_interface.py
@@ -9,35 +9,46 @@ class Login(QWidget):
 	def __init__(self, connection):
 		super().__init__()
 		self.setWindowTitle('BitsOJ v1.0.1 [ LOGIN ]')
-		self.resize(400, 300)
-		#self.setWindowFlag(Qt.WindowCloseButtonHint, False)
-		self.connection_object  = connection
-	
-		# Widget for taking username 
-		username_label = QLabel(self)
-		username_label.setText('USERNAME : ')
+		self.resize(700, 600)
 
-		self.username = QLineEdit(self)
-		self.username.setFixedWidth(100)
+		qtRectangle = self.frameGeometry()
+		centerPoint = QDesktopWidget().availableGeometry().center()
+		qtRectangle.moveCenter(centerPoint)
+		self.move(qtRectangle.topLeft())
 
+		self.title = QLabel('<<BitsOJ>>')
+		self.title.setFixedWidth(400)
+		self.title.setFixedHeight(150)
 		
-		password_label = QLabel(self)
-		password_label.setText('PASWORD : ')
+		self.username = QLineEdit(self)
+		self.username.setFixedWidth(400)
+		self.username.setFixedHeight(50)
+		self.username.setPlaceholderText('Username')
 
 		self.password = QLineEdit(self)
-		self.password.setFixedWidth(100)
-
+		self.password.setFixedWidth(400)
+		self.password.setFixedHeight(50)
+		self.password.setPlaceholderText('Password')
+		self.password.setEchoMode(QLineEdit.Password)
 
 		self.button_login = QPushButton('Login', self)
-		self.button_login.setFixedWidth(200)
+		self.button_login.setFixedWidth(300)
+		self.button_login.setFixedHeight(80)
 		self.button_login.clicked.connect(self.handle_login)
+		self.button_login.setDefault(True)
 
-		layout = QFormLayout(self)
-		layout.addRow(username_label, self.username)
-		layout.addRow(password_label, self.password)
-		layout.addRow(self.button_login)
-		layout.setContentsMargins(100, 50, 100, 50)
+		layout = QVBoxLayout(self)
+
+		layout.addWidget(self.title)
+		layout.addWidget(self.username)
+		layout.addWidget(self.password)
+		layout.addWidget(self.button_login)
+
+		
+		layout.setContentsMargins(150, 0, 0, 50)
+
 		self.setLayout(layout)
+		self.setObjectName('main')
 		self.show()
 		return
 
@@ -71,10 +82,8 @@ class start_interface(Login):
 	def __init__(self, connection):
 		app = QApplication(sys.argv)
 		app.setStyle("Fusion")
-		app.setStyleSheet(open('Elements/style.qss', "r").read())
+		app.setStyleSheet(open('Elements/login.qss', "r").read())
 		app.aboutToQuit.connect(self.closeEvent)
-		# Login.channel = channel
-		# Login.host = host
 		# make a reference of App class
 		login_app = Login(connection)
 		
