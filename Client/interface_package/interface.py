0ba17a381e82517e2a17d91518981ee0c2fbac4a
sachinam1397 <sachinam1397@gmail.com>
2019-11-08 22:52:11 +0530

    Contest Save

--- Client/interface_package/interface.py
+++ Client/interface_package/interface.py
@@ -30,6 +30,7 @@ class client_window(QMainWindow):
 		super().__init__()
 		global current_status
 		current_status = "STOPPED"
+		global Timer
 		# Set app icon
 		self.setWindowIcon(QIcon('Elements/logo.png'))
 		# Set window title
@@ -50,7 +51,8 @@ class client_window(QMainWindow):
 		if config["Contest"] == "RUNNING":
 			self.setWindowTitle('BitsOJ v1.0.1 [ CLIENT ][ RUNNING ]')
 			current_status = "RUNNING"
-			self.data_changed_flag[0] = 1
+			Timer = config["Duration"]
+			self.data_changed_flag[0] = 2
 			self.data_changed_flag[4] = 2
 		self.queue = queue
 
@@ -284,14 +286,18 @@ class client_window(QMainWindow):
 
 			if(self.data_changed_flag[4] == 1):
 				try:
+					config = handle_config.read_config_json()
 					total_time = initialize_contest.return_contest_end_time()
 					current_time = time.time()
+					end_time = config["End Time"]
 					elapsed_time = time.strftime('%H:%M:%S', time.gmtime(total_time - current_time ))
-					self.timer_widget.display(elapsed_time)
-					self.set_status()
-					if elapsed_time == '00:00:00':
+					if end_time <= current_time:
 						self.data_changed_flag[4] = 0
 						self.data_changed_flag[0] = 3
+						self.timer_widget.display('00:00:00')
+					else:
+						self.timer_widget.display(elapsed_time)
+						self.set_status()
 				except Exception as Error:
 					print(str(Error))
 
