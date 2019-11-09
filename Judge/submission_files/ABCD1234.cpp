e79543a8bddd3566032702fa517e96dd6204f767
ps0798 <prashant0797@gmail.com>
2019-11-10 02:37:51 +0530

    verdict

--- Judge/submission_files/ABCD1234.cpp
+++ Judge/submission_files/ABCD1234.cpp
@@ -4,11 +4,11 @@ using namespace std;
 typedef long long int lli;
 int main()
 {  
-	int t = 5;
-		while(1)
-		{
-			printf("5");
-		}
-    return 0;
-    
+	int t=100;
+	int a=0,b=0;
+	while(1)
+	{
+		// int x= a/b;
+		printf("5\n");
+	}
 }
