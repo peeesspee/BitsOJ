2a9011b3b935f6c27f83b8b88023a7a6033f55a3
sachinam1397 <sachinam1397@gmail.com>
2020-01-22 00:33:03 +0530

    Windows Client Update

--- Client_Windows/output/client/reset.py
+++
@@ -1,8 +0,0 @@
-from database_management import manage_database
-
-rabbitmq_username = 'client'
-rabbitmq_password = 'client'
-host = 'localhost'
-
-conn, cur = manage_database.initialize_table()
-manage_database.reset_database(conn)
