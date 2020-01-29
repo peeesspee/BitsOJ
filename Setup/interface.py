f55e85cc703c04a63faaab7f6eb5478091535515
Valiant1011 <prakharp789@gmail.com>
2020-01-29 20:38:40 +0530

    Setup v2.1.2

--- Setup/interface.py
+++ Setup/interface.py
@@ -11,8 +11,26 @@ class main_window(QWizard):
 		super(main_window, self).__init__(parent)
 		self.setStyleSheet(open('Elements/style.qss', "r").read())
 		self.setWindowIcon(QIcon('Elements/logo.png'))
-
 		self.config = config
+
+		self.selected_languages = {}
+		languages = self.config["Languages"]
+		allowed_languages = self.config.get("Allowed Languages", ["C"])
+		for language in languages:
+			if language in allowed_languages:
+				self.selected_languages[language] = "TRUE"
+			else:
+				self.selected_languages[language] = "FALSE"
+
+			self.ranklist_states = {}
+		ranklists = self.config.get("Available Ranklists", ["IOI"])
+		config_selected_rankist = self.config.get("Selected Ranklist", "IOI")
+		for ranklist in ranklists:
+			if ranklist == config_selected_rankist:
+				self.ranklist_states[ranklist] = "TRUE"
+			else:
+				self.ranklist_states[ranklist] = "FALSE"
+
 		self.home_page = wizard_page(config, 1)
 		self.addPage(self.home_page)
 		self.contest_page = wizard_page(config, 2)
@@ -29,7 +47,9 @@ class main_window(QWizard):
 		self.addPage(self.all_done_page)
 		
 		self.setWindowTitle("BitsOJ v1.1 [ CONTEST SETUP ]")
-		self.setFixedSize(1366, 768)
+		self.setFixedSize(1200, 750)
+
+		self.currentIdChanged.connect(self.page_changed_handler)
 
 		server_username = self.field("Server_Username")
 		server_password = self.field("Server_Password")
@@ -37,9 +57,32 @@ class main_window(QWizard):
 		client_password = self.field("Client_Password")
 		judge_username = self.field("Judge_Username")
 		judge_password = self.field("Judge_Password")
+		host = self.field("Host")
 		contest_name = self.field("Contest_Name")
 		contest_theme = self.field("Contest_Theme")
 		admin_key = self.field("Admin_Key")
 
+	def page_changed_handler(self, page):
+		if page == -1:
+			print('[ SETUP ] Cancelled')
+		elif page == 0:
+			print('[ SETUP ] Home Page View')
+		elif page == 1:
+			print('[ SETUP ] Contest Settings View')
+		elif page == 2:
+			print('[ SETUP ] Contest Settings Over')
+			print('[ SETUP ] RabbitMQ Page View')
+		elif page == 3:
+			print('[ SETUP ] RabbitMQ Settings Over')
+			print('[ SETUP ] Problems Page View')
+		elif page == 4:
+			print('[ SETUP ] Problems Added')
+			print('[ SETUP ] Language Page View')
+		elif page == 5:
+			print('[ SETUP ] Languages selected: ', self.selected_languages)
+			print('[ SETUP ] Ranking Page View')
+			# WORKING
+		elif page == 6:
+			# Do final config creation here
+			print('[ SETUP ] Finish')
 			
-		
--- Setup/ui_widgets.py
+++ Setup/ui_widgets.py
@@ -1,14 +1,14 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPalette, QColor, QPixmap
 from PyQt5.QtSql import QSqlTableModel, QSqlDatabase, QSqlQueryModel
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject, QTimer, Qt, QModelIndex, qInstallMessageHandler, pyqtProperty
+from PyQt5.QtCore import *
+import socket
 
 # This class creates pages for the setup wizard, based on the page id given to it.
 class wizard_page(QWizardPage):
 	def __init__(self, config, page_id = 1, parent=None):
 		super(wizard_page, self).__init__(parent)
 		self.config = config
-
 		self.top_bar = QWidget()
 		self.top_bar_layout = QHBoxLayout(self.top_bar)
 		self.logo = QLabel()
@@ -54,10 +54,10 @@ class wizard_page(QWizardPage):
 		self.welcome_label.setObjectName('main_screen_sub_heading')
 		message = (
 			"This utility will help you to generate configuration " +
-			"files for the Client, Server, and the Judge, absolutely hassle free.\n" +
-			"Many of the details will remain same if you are using the same setup to host " +
+			"files for the Client, Server, and the Judge,\nabsolutely hassle free." +
+			"Many of the details will remain same if you are using the same setup to\nhost " +
 			"multiple contests ( Recommended! ).\nAll the Best for your contest, and many more to come!\n" + 
-			" \t\t\t\t\t\t\t\t\t- Team Bitwise @ JUET, Guna"
+			" \t\t\t\t\t\t\t- Team Bitwise @ JUET, Guna"
 		)
 		self.message_label = QLabel(message)
 		self.message_label.setObjectName('main_screen_content')
@@ -101,7 +101,7 @@ class wizard_page(QWizardPage):
 		self.sub_head_1 = QLabel('Contest Decoration')
 		self.sub_head_1.setObjectName('main_screen_sub_heading')
 
-		self.contest_name_label = QLabel('&Contest Name: ')
+		self.contest_name_label = QLabel('&Contest Name:   ')
 		self.contest_name_label.setObjectName('main_screen_content')
 		self.contest_name_entry = QLineEdit()
 		self.contest_name_entry.setToolTip('Contest title')
@@ -167,7 +167,10 @@ class wizard_page(QWizardPage):
 		self.title_label = QLabel('RabbitMQ Connections')
 		self.title_label.setObjectName('main_screen_heading')
 
-		self.message_rabbitmq = QLabel('Fill in the account details of Server, Client and Judge, which were created in the RabbitMQ Management Portal: ')
+		self.message_rabbitmq = QLabel(
+			'Fill in the account details of Server, Client and Judge, which were ' +
+			'created in the RabbitMQ\nManagement Portal: '
+		)
 		self.message_rabbitmq.setObjectName('main_screen_content')
 
 		self.server_label = QLabel('Server')
@@ -264,6 +267,20 @@ class wizard_page(QWizardPage):
 			self.judge_password_widget
 		)
 
+		self.host_label = QLabel('Host:  ')
+		self.host_label.setObjectName('main_screen_sub_heading')
+
+		self.host_entry = QLineEdit()
+		self.host_entry.setToolTip('RabbitMQ Host IP Address')
+		self.host_entry.setText(self.config.get("Host", "localhost"))
+		self.registerField("Host", self.host_entry)
+		self.automatic_switch = QCheckBox('This PC')
+		self.automatic_switch.stateChanged.connect(self.ip_switch_change)
+		self.host_widget = self.get_horizontal_widget(
+			self.host_entry,
+			self.automatic_switch
+		)
+		
 		self.main_widget = QWidget()
 		self.main_widget.setObjectName('content_box')
 		self.main_layout = QVBoxLayout(self.main_widget)
@@ -279,7 +296,21 @@ class wizard_page(QWizardPage):
 		self.main_layout.addWidget(self.judge_label)
 		self.main_layout.addWidget(self.judge_creds_widget)
 		self.main_layout.addStretch(1)
+		self.main_layout.addWidget(self.host_label)
+		self.main_layout.addWidget(self.host_widget)
+		self.main_layout.addStretch(1)
 		return self.main_widget
+
+	def ip_switch_change(self, state):
+		if state == Qt.Checked:
+			print('[ SETUP ] Fetch IP')
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect(("8.8.8.8", 80))
+			ip = s.getsockname()[0]
+			s.close()
+			self.host_entry.setText(ip)
+		else:
+			self.host_entry.setText('localhost')
 
 	def problems_page(self):
 		self.title_label = QLabel('Problems')
@@ -296,68 +327,170 @@ class wizard_page(QWizardPage):
 	def languages_page(self):
 		self.title_label = QLabel('Languages')
 		self.title_label.setObjectName('main_screen_heading')
-		
-		# All the languages that are in the Judge
-		languages = self.config.get("Languages", ["TEXT"])
-		number_of_languages = len(languages)
-		# All the languages that are selected already
-		allowed_languages = self.config.get("Allowed Languages", ["TEXT"])
-		language_widgets_list = []
 
+		self.message_languages = QLabel(
+			'Set the allowed languages in the contest.' + 
+			'\nPlease check if you have a compiler available for the language you select.'
+		)
+		self.message_languages.setObjectName('main_screen_content')
+
+		# All the languages that are in the Judge
+		self.languages = self.config.get("Languages", ["TEXT"])
+		self.number_of_languages = len(self.languages)
+		# All the languages that were selected earlier, read from the config file
+		# Assuming this is a restart of setup, else this list is empty by default
+		self.allowed_languages = self.config.get("Allowed Languages", ["TEXT"])
+		# This list holds all the checkbox widgets generated ahead
+		self.language_widgets_list = []
+		# This widget holds all the checkboxes for the view
 		self.language_widget = QWidget()
 		self.language_layout = QVBoxLayout(self.language_widget)
-
-		for i in range(0, number_of_languages):
-			language_checkbox = QCheckBox(languages[i])
-			language_widgets_list.append(language_checkbox)
-
-			if languages[i] in allowed_languages:
-				language_widgets_list[i].setChecked(True)
+		# Mapper for checkbox signals, Read the clarification ahead
+		self.list_signal_mapper = QSignalMapper()
+		for i in range(0, self.number_of_languages):
+			language_checkbox = QCheckBox(self.languages[i])
+			self.language_widgets_list.append(language_checkbox)
+			if self.languages[i] in self.allowed_languages:
+				self.language_widgets_list[i].setChecked(True)
 			else:
-				language_widgets_list[i].setChecked(False)
-
-			language_widgets_list[i].stateChanged.connect(
-				lambda: self.language_checkbox_state_change_handler(
-					languages[i],
-					language_widgets_list[i].checkState()
+				self.language_widgets_list[i].setChecked(False)
+			self.language_layout.addWidget(self.language_widgets_list[i])
+		# Map each indexed widget to a mapper function which can handle state changes based on index
+		# WRONG : widget_list[i].stateChanged.connect(function) : This only selects the last widget in the list
+		# CORRECT : widget_list[] -----> mapper[] ----> function
+		for language_widget in self.language_widgets_list:
+			self.list_signal_mapper.setMapping(
+				language_widget, 
+				self.language_widgets_list.index(
+					language_widget
 				)
 			)
-			self.language_layout.addWidget(language_widgets_list[i])
-
+			language_widget.stateChanged.connect(self.list_signal_mapper.map)
+		# Now the signal mapper connects the checkbox to appropiae function call
+		self.list_signal_mapper.mapped[int].connect(
+			self.language_checkbox_state_change_handler
+		)
 		self.main_widget = QWidget()
 		self.main_widget.setObjectName('content_box')
 		self.main_layout = QVBoxLayout(self.main_widget)
 		self.main_layout.addWidget(self.title_label)
+		self.main_layout.addWidget(self.message_languages)
 		self.main_layout.addWidget(self.language_widget)
 		self.main_layout.addStretch(1)
 		return self.main_widget
 
-	def language_checkbox_state_change_handler(self, language, state):
-			if(state == Qt.Checked):
-				# Allow scoreboard update
-				print(language, "Allowed")
-			else:
-				# Stop scoreboard update
-				print(language, "Not Allowed")
-			return
+	def language_checkbox_state_change_handler(self, index):
+		widget = self.language_widgets_list[index]
+		state = widget.checkState()
+		if state == Qt.Checked:
+			print("[ SETUP ] Language " + self.languages[index], " : Allowed")
+			self.wizard().selected_languages[self.languages[index]] = "TRUE"
+		else:
+			print("[ SETUP ] Language " + self.languages[index], " : Not Allowed")
+			self.wizard().selected_languages[self.languages[index]] = "FALSE"
 
 	def ranking_page(self):
 		self.title_label = QLabel('Ranking')
 		self.title_label.setObjectName('main_screen_heading')
+
+		self.message_ranklist = QLabel(
+			'Set the Ranking style in the contest.'
+		)
+		self.message_ranklist.setObjectName('main_screen_content')
+
+		self.rbutton_signal_mapper = QSignalMapper()
+		self.ranklist_rbutton_list = []
+		
+		self.available_ranklists = self.config.get("Ranking Styles")
+		self.selected_ranklist = self.config.get("Selected Ranklist")
+
+		self.ranklist_widget = QWidget()
+		self.ranklist_layout  = QVBoxLayout(self.ranklist_widget)
+
+		for ranklist in self.available_ranklists:
+			ranklist_radiobutton = QRadioButton(ranklist)
+			if ranklist == self.selected_ranklist:
+				ranklist_radiobutton.setChecked(True)
+			self.ranklist_layout.addWidget(ranklist_radiobutton)
+			self.ranklist_rbutton_list.append(ranklist_radiobutton)
+			self.ranklist_layout.addStretch(1)
+		self.ranklist_layout.addStretch(1)
+
+		for ranklist in self.ranklist_rbutton_list:
+			self.rbutton_signal_mapper.setMapping(
+				ranklist, 
+				self.ranklist_rbutton_list.index(
+					ranklist
+				)
+			)
+			ranklist.toggled.connect(self.rbutton_signal_mapper.map)
+		# Now the signal mapper connects the checkbox to appropiae function call
+		self.rbutton_signal_mapper.mapped[int].connect(
+			self.ranklist_rbutton_state_change_handler
+		)
+
+		problem_config_score = self.config.get("Problem Max Score", 100)
+		self.problem_max_score_label = QLabel('Problem Score:')
+		self.problem_max_score = QLineEdit()
+		self.problem_max_score.setPlaceholderText("Max Score")
+		self.problem_max_score_widget = self.get_horizontal_widget(
+			self.problem_max_score_label,
+			self.problem_max_score
+		)
+		self.registerField("Problem Score", self.problem_max_score)
+
+		penalty_config_score = self.config.get("Penalty Score", 0)
+		self.penalty_score_label = QLabel('Penalty Score:  ')
+		self.penalty_score = QLineEdit()
+		self.penalty_score.setPlaceholderText("No Penalty")
+		self.penalty_score_widget = self.get_horizontal_widget(
+			self.penalty_score_label,
+			self.penalty_score
+		)
+		self.registerField("Penalty Score", self.penalty_score)
+		
+		penalty_config_score = self.config.get("Penalty Time", 0)
+		self.penalty_time_label = QLabel('Penalty Time:   ')
+		self.penalty_time = QLineEdit()
+		self.penalty_time.setPlaceholderText("0 Minutes")
+		self.penalty_time_widget = self.get_horizontal_widget(
+			self.penalty_time_label,
+			self.penalty_time
+		)
+		self.registerField("Penalty Time", self.penalty_time)
+		
+		self.ranklist_layout.addWidget(self.problem_max_score_widget)
+		self.ranklist_layout.addWidget(self.penalty_score_widget)
+		self.ranklist_layout.addWidget(self.penalty_time_widget)
+
+
 		
 		self.main_widget = QWidget()
 		self.main_widget.setObjectName('content_box')
 		self.main_layout = QVBoxLayout(self.main_widget)
 		self.main_layout.addWidget(self.title_label)
-	
-		self.main_layout.addStretch(1)
+		self.main_layout.addWidget(self.message_ranklist)
+		self.main_layout.addWidget(self.ranklist_widget)
+		self.main_layout.setStretch(0, 10)
+		self.main_layout.setStretch(1, 10)
+		self.main_layout.setStretch(2, 80)
 		return self.main_widget
+
+	def ranklist_rbutton_state_change_handler(self, index):
+		widget = self.ranklist_rbutton_list[index]
+		state = widget.isChecked()
+		if state == True:
+			print("[ SETUP ] Ranklist " + self.available_ranklists[index], " : Selected")
+			self.wizard().ranklist_states[self.available_ranklists[index]] = "TRUE"
+		else:
+			self.wizard().ranklist_states[self.available_ranklists[index]] = "FALSE"
 
 	def all_done_page(self):
 		self.title_label = QLabel('Finalize')
 		self.title_label.setObjectName('main_screen_heading')
 		message = (
-			"You're all done here!\nCopy the generated config files into Server, Client and Judge folders and proceed to start the Server."
+			"You're all done here!\nCopy the generated config files into Server, Client and Judge folders\n" + 
+			"and proceed to start the Server."
 		)
 		self.all_done_content = QLabel(message)
 		self.all_done_content.setObjectName('main_screen_content')
